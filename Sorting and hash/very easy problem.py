3b61740e20ee7a0d4a8ad2f1f91c8e318040817a
danila-kopitayko <dan.kopitajko@yandex.ru>
2025-11-04 10:29:08 +0300

    Update very easy problem

--- Sorting and hash/very easy problem.py
+++ Sorting and hash/very easy problem.py
@@ -10,19 +10,45 @@ def solution(x,y,n):
             l = m
         else:
             r = m
-    return r + min(x,y)
+    return l + min(x,y)
 
 def solution_lcm(x,y,n):
     lcm = math.lcm(x,y)
     amount = math.floor(n*x*y//(lcm*(x + y)))
-    time = lcm*amount
-    count = 0
-    print('asdfasdf',amount)
-    while amount<n:
-        print(f'amount={amount},time={time}')
-        amount+=1
-        time+=min(x,y)
-    return time+min(x,y)
+    print(f'lcm={lcm}')
+    #time = lcm*amount
+    time_big = 0
+    time_small = 0
+    if amount*(lcm/x + lcm/y)>=n-1:
+        count=0
+    else:
+        count = amount*(lcm/x + lcm/y)
+    rest = (n - count - 1)*min(x,y)
+    time = 0
+    print(f'beginning:\ntime={time}\ncount={count}\nrest={rest}')
+    additional_time = 0
+    if count<n-1 and rest//max(x,y)==0:
+        count+=n-count - 1
+        time+=rest
+        print(f'rest//max(x,y)==0:\ntime={time}\ncount={count}')
+        return time
+    elif rest//max(x,y)!=0:
+        time+=(rest//max(x,y)) * max(x,y)
+        time_big+=1
+        count+=(rest//max(x,y))
+        print(f'rest//max(x,y)!=0:\ntime={time}\ncount={count}')
+    while count<n:
+        #time+=min(x,y)
+        time_small+=1
+        count+=1
+        if time_small*min(x,y)>time_big*max(x,y):
+            time+=time_small*min(x,y)-time_big*max(x,y)
+    return time
+
+
+    count+=1
+    #print(time)
+    #return time+min(x,y)
 
 
     #print(time)
@@ -32,8 +58,8 @@ def solution_lcm(x,y,n):
 
 def main():
     iter = random.randint(5,15)
-    print(solution(5, 3, 9))
-    print(solution_lcm(5, 3, 9))
+    print(solution(2, 3, 5))
+    print(solution_lcm(2, 3, 5))
     '''    for i in range(iter):
         n = random.randint(0, 10)
         x = random.randint(1, 5)
