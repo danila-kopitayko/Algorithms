dc4ebc289792ae2d1430e559a4a19b2b97b3b039
danila-kopitayko <dan.kopitajko@yandex.ru>
2025-12-09 21:55:22 +0300

    Update max_palindrome.py

--- Dynamic Programming/max_palindrome.py
+++ Dynamic Programming/max_palindrome.py
@@ -1,6 +1,8 @@
 def test(string):
     n=len(string)
     max_length = 0
+    if n==1:
+        return 1
     for i in range(1,n-1):
         for j in range(2):
             l, r = i, i+j
@@ -12,13 +14,39 @@ def test(string):
 
     return max_length
 
-def solution():
+def solution(s):
+    n = len(s)
+    if n==1:
+        return 1
+    dp = [[False for i in range(n)] for i in range(n)]
+    count = 0
+    dp[n-1][n-1] = True
+    max_length = 0
+
+
+    for i in range(n-1):
+        dp[i][i] = True
+        if s[i]==s[i+1]:
+            dp[i][i+1]=True
+            if max_length<2:
+                max_length = 2
+    for length in range(3,n+1):
+        for i in range(n - length + 1):
+            j = i + length - 1
+            if s[i]==s[j] and dp[i+1][j-1]==True:
+                dp[i][j]=True
+                if max_length < length:
+                    max_length = length
+    return max_length
+
+
     
 
 
 def main():
-    s = 'babad'
+    s = 'b'
     print(test(s))
+    print(solution(s))
     return 0
 
 
